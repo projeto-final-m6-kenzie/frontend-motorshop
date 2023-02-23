6f331b7e17b131b4f793b798ba825f0b380d89a3
jefersonokdemais <jefersonokdemais@hotmail.com>
2023-02-23 13:46:44 -0300

    feat: carrocel in Leilão

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -2,40 +2,65 @@ import background from '../../assets/imgs/car1.png'
 import { Cards_carrossel } from '../Cards'
 import { Container, Container_carrossel } from './style'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Pagination } from 'swiper'
+
 const Carrossel = () => {
   return (
     <Container>
-      <div>
+{/*       <div>
         <button>D</button>
         <button>D</button>
-      </div>
+      </div> */}
 
       <Container_carrossel>
-        <Cards_carrossel
-          img={background}
-          title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
-          info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
-        />
-        <Cards_carrossel
-          img={background}
-          title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
-          info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
-        />
-        <Cards_carrossel
-          img={background}
-          title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
-          info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
-        />
-        <Cards_carrossel
-          img={background}
-          title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
-          info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
-        />
-        <Cards_carrossel
-          img={background}
-          title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
-          info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
-        />
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className='mySwiper'
+        >
+          <SwiperSlide>
+            <Cards_carrossel
+              img={background}
+              title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
+              info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Cards_carrossel
+              img={background}
+              title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
+              info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Cards_carrossel
+              img={background}
+              title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
+              info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Cards_carrossel
+              img={background}
+              title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
+              info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Cards_carrossel
+              img={background}
+              title='Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 '
+              info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...'
+            />
+          </SwiperSlide>
+        </Swiper>
       </Container_carrossel>
     </Container>
   )
