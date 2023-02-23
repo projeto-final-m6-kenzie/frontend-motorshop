08b450aa277b2cbbe59f19f38de111fe30eee006
Rayan Brasil <brasrayan@gmail.com>
2023-02-23 09:18:24 -0400

    feat:formulario de registro

---
+++ src/components/RegisterForm/index.tsx
@@ -0,0 +1,77 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import * as yup from 'yup'
+
+// const schema = yup.object().shape({
+//     name: ,
+//     email: ,
+//     cpf: ,
+//     phone: ,
+//     birthdate: ,
+//     description: ,
+//     password: ,
+//     cep: ,
+//     state: ,
+//     city: ,
+//     street: ,
+//     number: ,
+//     complement: ,
+// })
+
+const RegisterForm = () => {
+  return (
+    <form>
+      <h1>Cadastro</h1>
+      <p>Informações pessoais</p>
+
+      <label htmlFor='name'>Nome</label>
+      <input placeholder='Ex.: Samuel Leão'></input>
+
+      <label htmlFor='email'>Email</label>
+      <input placeholder='Ex.: samuel@kenzie.com.br'></input>
+
+      <label htmlFor='cpf'>CPF</label>
+      <input placeholder='000.000.000-00'></input>
+
+      <label htmlFor='phone'>Celular</label>
+      <input placeholder='(DDD) 90000-0000'></input>
+
+      <label htmlFor='birthdate'>Data de nascimento</label>
+      <input placeholder='00/00/00'></input>
+
+      <label htmlFor='birthdate'>Descrição</label>
+      <textarea placeholder='Digitar descrição'></textarea>
+
+      <span>Informações de endereço</span>
+
+      <label htmlFor='cep'>CEP</label>
+      <input placeholder='00000.000'></input>
+
+      <label htmlFor='state'>Estado</label>
+      <input placeholder='Digitar Estado'></input>
+
+      <label htmlFor='city'>Cidade</label>
+      <input placeholder='Digitar cidade'></input>
+
+      <label htmlFor='street'>Rua</label>
+      <input placeholder='Digitar rua'></input>
+
+      <label htmlFor='number'>Número</label>
+      <input placeholder='Digitar número'></input>
+
+      <label htmlFor='complement'>Complemento</label>
+      <input placeholder='Ex.: apart 307'></input>
+
+      <p>TIPO DE CONTA</p>
+
+      <label htmlFor='password'>Senha</label>
+      <input type='password' placeholder='Digitar senha'></input>
+
+      <label htmlFor='passwordConf'>Confirmar Senha</label>
+      <input type='password' placeholder='Digitar senha'></input>
+    </form>
+  )
+}
+
+export default RegisterForm
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes, BrowserRouter } from 'react-router-dom'
 
+import RegisterForm from '../components/RegisterForm'
 import Home from '../pages/Home'
 
 const RoutesMain = () => (
   <Routes>
-    <Route path='/' element={<Home />} />
+    <Route path='/' element={<RegisterForm />} />
     <Route path='*' element={<Navigate to='/' />} />
   </Routes>
 )
