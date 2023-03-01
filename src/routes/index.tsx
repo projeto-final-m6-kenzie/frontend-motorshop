f05faec885506fc59736f9cc19713c6f9c4de020
Rayan Brasil <brasrayan@gmail.com>
2023-03-01 13:58:27 -0400

    feat: formulário de editar perfil

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Login from '../pages/Login'
 import Product from '../pages/Products'
 import ProfileViewUser from '../pages/ProfileViewUser'
 import Register from '../pages/Register'
+import UpdateUserForm from '../pages/UpdateProfile'
 
 const RoutesMain = () => (
   <Routes>
@@ -13,6 +14,7 @@ const RoutesMain = () => (
     <Route path='/product' element={<Product />} />
     <Route path='/login' element={<Login />} />
     <Route path='/register' element={<Register />} />
+    <Route path='/update' element={<UpdateUserForm />} />
     <Route path='*' element={<Navigate to='/' />} />
   </Routes>
 )
